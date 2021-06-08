257e25bd797d30a913e3befe7b4b348ff3ed1a4e
Gregory Blood <gregoryblood1998@gmail.com>
2021-06-07 17:12:23 -0700

    half is working

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const redis = require('redis');
 const morgan = require('morgan');
 const api = require('./api');
 const { connectToDB } = require('./lib/mongo');
 
 const app = express();
+
+
+const redisClient = redis.createClient(
+  process.env.REDIS_PORT || '6379',
+  process.env.REDIS_HOST || 'localhost'
+);
+
+const rateLimitWindowMS = 60000;
+const rateLimitMaxRequests = 5;
 
 const port = process.env.PORT || 8000;
 
@@ -13,6 +23,69 @@ const port = process.env.PORT || 8000;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
+
+
+
+
+function getUserTokenBucket(ip) {
+  return new Promise((resolve, reject) => {
+    redisClient.hgetall(ip, (err, tokenBucket) => {
+      if (err) {
+        reject(err);
+      } else if (tokenBucket) {
+        tokenBucket.tokens = parseFloat(tokenBucket.tokens);
+        resolve(tokenBucket);
+      } else {
+        resolve({
+          tokens: rateLimitMaxRequests,
+          last: Date.now()
+        });
+      }
+    });
+  });
+}
+
+function saveUserTokenBucket(ip, tokenBucket) {
+  return new Promise((resolve, reject) => {
+    redisClient.hmset(ip, tokenBucket, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function rateLimit(req, res, next) {
+  try {
+    const tokenBucket = await getUserTokenBucket(req.ip);
+
+    const currentTimestamp = Date.now();
+    const ellapsedTime = currentTimestamp - tokenBucket.last;
+    tokenBucket.tokens += ellapsedTime *
+      (rateLimitMaxRequests / rateLimitWindowMS);
+    tokenBucket.tokens = Math.min(
+      tokenBucket.tokens,
+      rateLimitMaxRequests
+    );
+    tokenBucket.last = currentTimestamp;
+
+    if (tokenBucket.tokens >= 1) {
+      tokenBucket.tokens -= 1;
+      await saveUserTokenBucket(req.ip, tokenBucket);
+      next();
+    } else {
+      res.status(429).send({
+        error: "Too many request per minute.  Please wait a bit..."
+      });
+    }
+  } catch (err) {
+    next();
+  }
+}
+
+app.use(rateLimit);
 
 /*
  * All routes for the API are written in modules in the api/ directory.  The
