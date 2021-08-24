db9ec98e98d77fbf383c0d2e440450c2891d0ef0
Zach Kauffman <zach.kauffman0@gmail.com>
2021-08-23 22:56:05 -0700

    added state to pear button

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import NavBar from './NavBar';
 import './App.css';
 import Button from './Button';
 
-function App() {
-    return (
-        <div className="App">
-            <NavBar />
-            <Button 
-                icon="https://freeiconshop.com/wp-content/uploads/edd/pear-outline.png"
-            />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pearButtonPressed: false,
+        };
+    }
+
+    handlePearButtonClick() {
+        this.setState({pearButtonPressed: !this.state.pearButtonPressed})
+        let status = this.state.pearButtonPressed ? "is" : "is not";
+        console.log("Pear button " + status + " pressed.");
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <NavBar />
+                <Button 
+                    icon="https://freeiconshop.com/wp-content/uploads/edd/pear-outline.png"
+                    onClick={() => this.handlePearButtonClick()}
+                />
+            </div>
+        );
+    } 
 }
 
 export default App;
